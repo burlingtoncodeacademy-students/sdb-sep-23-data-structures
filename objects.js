6322cf2de0002243059319df6a57400065109f06
Paul Niemczyk <mrpndev@gmail.com>
2023-09-26 11:40:08 -0400

    Objects Lecture Complete

--- objects.js
+++ objects.js
@@ -83,3 +83,106 @@ console.log(porsche)
     * Matching users and passwords between database and incoming object
 */
 
+const request = {
+    username: "paull",
+    password: "dbLocal"
+}
+
+const db = [
+    { username: "paul" , password: "dbLocal" },
+    { username: "bhoffey", password: "ilikecars123" },
+    { username: "joey", password: "potato123" },
+    { username: "mposnik", password: "password " }
+]
+
+// ? Build a login function
+// ? Check if the user exists (by matching their username to one in the db)
+// ? if they don't exist, return "user does not exist"
+// ? if they do exist, check the password
+// ? if password incorrect, return "incorrect password", otherwise "user logged in"
+
+const login = () => {
+    const foundUser = db.filter(usr => request.username === usr.username)
+    console.log(foundUser)
+    // if (!foundUser[0]) {
+    //     console.log("User not found")
+    // } else {
+    //     if (foundUser[0].password !== request.password) {
+    //         console.log("Incorrect password")
+    //     } else {
+    //         console.log("Logged in")
+    //     }
+    // }
+
+    try {
+        if (!foundUser[0]) throw new Error("User not found")
+    
+        if (foundUser[0].password !== request.password) throw new Error("Incorrect password")
+
+        console.log("Logged in")
+    } catch(err) {
+        console.log(err.message)
+    }
+
+    console.log("Code to execute after Guard Clauses")
+}
+
+login()
+
+/* 
+    ! Challenge
+    * add season3
+    * it will hold properties of numberOfEpisodes and episodeInfo
+    * episodeInfo will be an array of 2 objects
+    * the two objects will have properties of episode & episodeName
+    * fabricate their values
+    ! Spicey Mode
+    * numberOfEpisodes should auto-calculate how many there are instead of hardcoding the value
+*/
+
+let netflix = {
+    id: 9,
+    likes: 932,
+    name: "The Good Place",
+    seasonInfo: {
+        season1: {
+            numberOfEpisodes: 5,
+            episodeInfo: [
+                { episode: 1, episodeName: "Pilot"},
+                { episode: 2, episodeName: "Flying"},
+                { episode: 3, episodeName: "Tahani Al-Jamil"},
+                { episode: 4, episodeName: "Jason Mendoza"},
+                { episode: 5, episodeName: "Category 55 Emergency"},
+            ]
+        },
+        season2: {
+            numberOfEpisodes: 6,
+            episodeInfo: [
+                { episode: 1, episodeName: "Everything is Great"},
+                { episode: 2, episodeName: "Dance Dance Resolution"},
+                { episode: 3, episodeName: "Team Cockroach"},
+                { episode: 4, episodeName: "Existential Crisis"},
+                { episode: 5, episodeName: "The Trolley Problem"},
+            ]
+        }
+    },
+    isAvailable: true
+}
+
+// console.log(netflix.seasonInfo.season1.episodeInfo[0].episodeName)
+netflix.seasonInfo.season3 = {
+    episodeInfo: [
+        {episode: 1, episodeName: "Something"},
+        {episode: 2, episodeName: "Something else"}
+    ]
+}
+
+netflix.seasonInfo.season3.numberOfEpisodes = netflix.seasonInfo.season3.episodeInfo.length
+
+console.log(netflix.seasonInfo.season3)
+
+// ! Spicey Mode - in netflix object, create totalSeasons property with dynamic value of all seasons
+
+// console.log(Object.keys(netflix.seasonInfo).length)
+netflix.totalSeason = Object.keys(netflix.seasonInfo).length
+console.log(netflix)
